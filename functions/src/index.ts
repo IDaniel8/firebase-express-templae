59c59d1100944c5edfc89667454b39195b099298
Ali Daniel Febres T <alidanft@gmail.com>
2020-08-07 17:44:55 +0200

    polish missing scripts and deployment information

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,4 +2,4 @@ import 'module-alias/register';
 import * as functions from 'firebase-functions';
 import server from './server';
 
-exports.api = functions.runWith({ memory: '2GB', timeoutSeconds: 120 }).https.onRequest(server);
+exports.api = functions.region('europe-west2').runWith({ memory: '2GB', timeoutSeconds: 120 }).https.onRequest(server);
